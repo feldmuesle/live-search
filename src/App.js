3185d97a4a87de13ab8593756013a9903a2d8bb1
Lisa Felder <feldmuesle@yahoo.com>
2020-08-08 15:46:30 +0200

    install & setup of fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import setupIcons from './setup-icons';
 import './App.css';
+
+setupIcons();
 
 function App() {
   return <div className="App">Exiting stuff to come...</div>;
