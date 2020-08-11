bf6c0c37c899286622f9381564bfe7652e8328ef
Lisa Felder <feldmuesle@yahoo.com>
2020-08-11 20:47:46 +0200

    minor styling tweaks & cleanup

--- src/api/get-data.js
+++ src/api/get-data.js
@@ -1,11 +1,6 @@
 /* eslint-disable import/no-named-default */
 
-import { default as dummyData } from './data.json';
-
 const getData = (url) => {
-  /* temporarily until we've developed the drowdown' */
-  // return dummyData;
-
   return fetch(url)
     .then((response) => response.json())
     .then((json) => json);
